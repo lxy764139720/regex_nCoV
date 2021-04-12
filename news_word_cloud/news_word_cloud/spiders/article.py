85ad4a78f4c6a44db59d75f19e7c09b067f4707e
lxy764139720 <764139720@qq.com>
2021-04-13 01:05:05 +0800

    feat: 实验二：两会词云-爬取十三届全国人大四次会议数据

---
+++ news_word_cloud/news_word_cloud/spiders/article.py
@@ -0,0 +1,47 @@
+import re
+import scrapy
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+
+from news_word_cloud.items import Article
+
+
+class ArticleSpider(scrapy.Spider):
+    name = 'article'
+    allowed_domains = ['lianghui.people.com.cn']
+    start_urls = ['http://lianghui.people.com.cn/2021npc/GB/437015/index1.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437015/index2.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437015/index3.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437015/index4.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437033/index1.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437033/index2.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437033/index3.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437033/index4.html',
+                  'http://lianghui.people.com.cn/2021npc/GB/437033/index5.html']
+    rules = [Rule(LinkExtractor(allow=r'.*'),
+                  callback='parse',
+                  follow=True)]
+
+    def parse(self, response, **kwargs):
+        article = Article()
+        bs = BeautifulSoup(response.body, "lxml")
+        if bs.find('h1', {'id': 'p_title'}):
+            # 文章页面
+            article['url'] = response.url
+            article['title'] = bs.find('h1', {'id': 'p_title'}).get_text()
+            text = ''
+            for content in bs.find('div', {'id': 'p_content'}).children:
+                if content.name == 'p':
+                    # print(content.get_text())
+                    text += content.get_text()
+            article['text'] = text.replace('\n', '').replace('\r', '').replace('\t', '').replace(u'\xa0', u'').replace(u'\u3000', u'')
+            # 20210306
+            article['date'] = '2021' + re.findall(r'/2021npc/n1/2021/([0-9]{4})/[^/]+?.html', response.url)[0]
+            yield article
+        else:
+            # 索引页面
+            # print(response.body.decode('GBK'))
+            for url in re.findall(r'/2021npc/n1/2021/[0-9]{4}/[^/]+?.html', response.body.decode('GBK')):
+                # print(url)
+                yield scrapy.Request('http://lianghui.people.com.cn' + url, callback=self.parse)
